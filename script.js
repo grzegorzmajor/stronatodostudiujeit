3ceec438d3b7ddf1d49c2c978b5417a594e66e4a
grzegorzmajor <major.grzegorz@gmail.com>
2022-10-28 10:24:59 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonsDelete= document.querySelectorAll ('.delete');
+console.log(buttonsDelete);
+
+function deleteItem(e){ e.target.parentNode.remove(); }
+
+for(let i=0; i<buttonsDelete.length;i++) { buttonsDelete[i].addEventListener('click',deleteItem); }
